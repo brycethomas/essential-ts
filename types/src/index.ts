22de523a755960a58752733c5b96bc068bfcb560
Bryce Thomas <bryce.m.thomas@gmail.com>
2020-08-29 10:43:49 -0700

    [partial] Follow along for Chapter 10.
    Up to "Type Guarding with a Type Predicate Function".

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,120 +1,33 @@
-function calculateTax(amount: number): number {
-    return amount * 1.2;
-}
+enum Feature { Waterproof, Insulated }
 
-function writePrice(product: string, price: number): void {
-    console.log(`Price for ${product}: ${price.toFixed(2)}`);
-}
+type Product = {
+    id: number
+    name: string,
+    price?: number
+};
 
-function calculatePrice(quantity: 1 | 2, price: number): number {
-    return quantity * price;
-}
+type Person = {
+    id: string,
+    name: string,
+    city: string
+};
 
-let hatPrice = 100;
-let glovesPrice = 75;
-let umbrellaPrice = 42;
+type UnionType = {
+    id: number | string,
+    name: string
+};
 
-let prices: number[] = [100, 75, 42];
-let names: string[] = ["Hat", "Gloves", "Umbrella"];
+let hat = { id: 1, name: "Hat", price: 100 };
+let gloves = { id: 2, name: "Gloves", price: 75 };
+let umbrella = { id: 3, name: "Umbrella", price: 30 };
+let bob = { id: "bsmith", name: "Bob", city: "London" };
 
-writePrice("Hat", calculateTax(hatPrice));
-writePrice("Gloves", calculateTax(glovesPrice));
-writePrice("Umbrella", calculateTax(umbrellaPrice));
+let dataItems: (Product | Person)[] = [hat, gloves, umbrella, bob];
 
-writePrice(names[0], calculateTax(prices[0]));
-writePrice(names[1], calculateTax(prices[1]));
-writePrice(names[2], calculateTax(prices[2]));
-
-prices.forEach((price: number, index: number) => {
-    writePrice(names[index], calculateTax(price));
-});
-
-
-let hat: [string, number] = ["Hat", 100];
-let gloves: [string, number] = ["Gloves", 75];
-
-writePrice(hat[0], hat[1]);
-writePrice(gloves[0], gloves[1]);
-
-hat.forEach((h: string | number) => {
-    if (typeof h === "string") {
-        console.log(`String: ${h}`);
+dataItems.forEach(item => {
+    if ("city" in item) {
+        console.log(`Person: ${item.name}: ${item.city}`);
     } else {
-        console.log(`Number: ${h.toFixed(2)}`);
+        console.log(`Product: ${item.name}: ${item.price}`);
     }
 });
-
-enum Product { Hat, Gloves, Umbrella }
-
-[Product.Hat, Product.Gloves, Product.Umbrella].forEach(val => {
-    console.log(`Number value: ${val}`);
-});
-
-console.log('==============');
-
-let products: [Product, number][] = [[Product.Hat, 100], [Product.Gloves, 75]];
-
-products.forEach((prod: [Product, number]) => {
-    switch (prod[0]) {
-        case Product.Hat:
-            writePrice("Hat", calculateTax(prod[1]));
-            break;
-        case Product.Gloves:
-            writePrice("Gloves", calculateTax(prod[1]));
-            break;
-        case Product.Umbrella:
-            writePrice("Umbrella", calculateTax(prod[1]));
-            break;
-    }
-});
-
-// enum City { London = "London", Paris = "Paris", NY = "New York" }
-// console.log(`City: ${City.London}`);
-
-let restrictedValue: 1 | 2 | 3 = 1;
-let secondValue: 1 | 10 | 100 = 1;
-
-restrictedValue = secondValue;
-secondValue = 100;
-// restrictedValue = secondValue;
-
-console.log(`Value: ${restrictedValue}`);
-
-let total = calculatePrice(2, 19.99);
-console.log(`Price: ${total}`);
-
-function getRandomValue(): 1 | 2 | 3 | 4 {
-    return Math.floor(Math.random() * 4) + 1 as 1 | 2 | 3 | 4;
-}
-
-enum City { London = "LON", Paris = "PAR", Chicago = "CHI" }
-
-type comboType = [string, number | true, 1 | 2 | 3 | City.London][];
-
-function getValue(input: comboType): comboType {
-    return [["Apples", 100, 2], ["Oranges", true, 3]];
-}
-
-let result: comboType = getValue([["Bananas", true, 1]]);
-console.log(`Result: ${result}`);
-
-function getMixedValue(input: 1): 1;
-function getMixedValue(input: 2 | 3): "Hello" | true;
-function getMixedValue(input: 4): City.London;
-function getMixedValue(): 1 | "Hello" | true | City.London {
-    switch (getRandomValue()) {
-        case 1:
-            return 1;
-        case 2:
-            return "Hello";
-        case 3:
-            return true;
-        case 4:
-            return City.London;
-    }
-}
-
-let first = getMixedValue(1);
-let second = getMixedValue(2);
-let third = getMixedValue(4);
-console.log(`${first}, ${second}, ${third}`);
