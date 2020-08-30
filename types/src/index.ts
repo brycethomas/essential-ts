0f72a5261a55bcf6b3c9de0c20adf24c248cb0a5
Bryce Thomas <bryce.m.thomas@gmail.com>
2020-08-30 15:16:54 -0700

    Follow along with Chapter 12.

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,70 +1,44 @@
-abstract class Person {
-    constructor(public id: string, public name: string,
-        public city: string) { }
+import { City, Person, Product } from "./dataTypes";
 
-    getDetails(): string {
-        return `${this.name}, ${this.getSpecificDetails()}`;
+let people = [new Person("Bob Smith", "London"),
+new Person("Dora Peters", "New York")];
+
+let products = [new Product("Running Shoes", 100),
+new Product("Hat", 25)];
+
+let cities = [new City("London", 81360000),
+new City("Paris", 2141000)];
+
+class DataCollection<T extends { name: string }> {
+    private items: T[] = [];
+
+    constructor(initialItems: T[]) {
+        this.items.push(...initialItems);
     }
 
-    abstract getSpecificDetails(): string;
-}
-
-class Employee extends Person {
-    constructor(public readonly id: string, public name: string,
-        public dept: string, public city: string) {
-        // No statements required for plain old assignments
-        super(id, name, city);
+    add(newItem: T) {
+        this.items.push(newItem);
     }
 
-    writeDept() {
-        console.log(`${this.name} works in ${this.dept}`);
+    getNames(): string[] {
+        return this.items.map(item => item.name);
     }
 
-    getSpecificDetails() {
-        return `works in ${this.dept}`;
-    }
-}
-
-class Customer extends Person {
-    constructor(public readonly id: string, public name: string,
-        public city: string, public creditLimit: number) {
-        super(id, name, city);
-    }
-
-    getSpecificDetails() {
-        return `has ${this.creditLimit} limit`;
+    getItem(index: number): T {
+        return this.items[index];
     }
 }
 
-class Supplier extends Person {
-    constructor(public readonly id: string, public name: string,
-        public city: string, public companyName: string) {
-        super(id, name, city);
-    }
+let peopleData = new DataCollection<Person>(people);
+// console.log(`Names: ${peopleData.getNames().join(", ")}`);
+let firstPerson = peopleData.getItem(0);
+console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
+console.log(`Person Names: ${peopleData.getNames().join(", ")}`);
 
-    getSpecificDetails() {
-        return `works for ${this.companyName}`;
-    }
-}
+let productData = new DataCollection<Product>(products);
+let firstProduct = productData.getItem(0);
+console.log(`First Product: ${firstProduct.name}, ${firstProduct.price}`);
+console.log(`Product Names: ${productData.getNames().join(", ")}`);
 
-let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris");
-console.log(`Dept value: ${salesEmployee.dept}`);
-// salesEmployee.id = "fidel";
-
-let data: Person[] = [new Employee("fvega", "Fidel Vega", "Sales", "Paris"),
-new Customer("ajones", "Alice Jones", "London", 500)];
-
-data.push(new Supplier("dpeters", "Dora Peters", "New York", "Acme"));
-
-data.forEach(item => {
-    console.log(`Person: ${item.name}, ${item.city}`);
-    if (item instanceof Employee) {
-        item.writeDept();
-    } else if (item instanceof Customer) {
-        console.log(`Customer ${item.name} has ${item.creditLimit} limit`);
-    } else if (item instanceof Supplier) {
-        console.log(`Supplier ${item.name} works for ${item.companyName}`);
-    }
-});
-
-data.forEach(item => console.log(item.getDetails()));
+let cityData = new DataCollection<City>(cities);
+console.log(`City Names: ${cityData.getNames().join(", ")}`);
