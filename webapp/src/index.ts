898327cf6d70f7717658765a7b7b5699057bc22b
Bryce Thomas <bryce.m.thomas@gmail.com>
2020-09-05 14:32:54 -0700

    [partial] Follow along with Chapter 15.
    Completed chapter.  Note this is not successfully compiling so there is an error
    somewhere.

--- webapp/src/index.ts
+++ webapp/src/index.ts
@@ -10,8 +10,7 @@ let ds = new LocalDataSource();
 async function displayData(): Promise<HTMLElement> {
     let display = new HtmlDisplay();
     display.props = {
-        products: await ds.getProducts("name"),
-        order: ds.order
+        dataSource: ds
     }
     return display.getContent();
 }
